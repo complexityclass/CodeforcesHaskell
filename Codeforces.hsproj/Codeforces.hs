b1f580d052a6d199ca8fa81c4f7bdf3698fac7b4
Popov Valery <v.popov@rambler-co.ru>
2016-11-20 21:42:49 +0300

    initial commit

---
+++ Codeforces.hsproj/Codeforces.hs
@@ -0,0 +1,39 @@
+module Codeforces where
+  
+import Crypto.Hash.SHA1
+import qualified
+       Data.ByteString.Char8  as BS
+import Data.Char
+import Data.List
+import Numeric
+import System.Environment
+import System.Exit
+import System.IO
+
+-- Utils for test problems solutions in haskell for mac
+-- before submit change problemRead with getLine
+
+bail msg
+  = do
+    { hPutStrLn stderr msg
+    ; exitFailure
+    }
+    
+lonelyFileOption = "expected file name after '--file'"
+tooManyArguments = "too many arguments"
+
+problemRead :: IO (String)
+problemRead = do
+    { args <- getArgs
+    ; input <- case args of
+        []                -> do 
+                             { putStr "Enter text to hash: "
+                             ; hFlush stdout
+                             ; BS.getLine
+                             }
+        ["--file"]        -> bail lonelyFileOption
+        ["--file", fname] -> BS.readFile fname
+        [str]             -> return $ BS.pack str
+        _                 -> bail tooManyArguments
+    ; let inputString = BS.unpack input
+    ; return inputString }
---
+++ Codeforces.hsproj/TheatreSquare.hs
@@ -0,0 +1,15 @@
+module TheatreSquare where
+  
+import Codeforces
+import System.Environment
+
+--http://codeforces.com/problemset/problem/1/A?locale=en
+
+solution :: [Int] -> Int
+solution input = case input of
+  [n,m,a] -> div(-n)a*div(-m)a
+  _       -> 0
+
+main = do
+  [n, m, a] <- fmap (map read . words) problemRead
+  print $ ceiling (n / a) * ceiling (m / a)
